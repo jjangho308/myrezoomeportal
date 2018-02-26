d65109243899f2f3de8dece8b4291d2136fcb4b1
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-02-26 22:07:56 +0900

    이동용

---
+++ modules/property.js
@@ -0,0 +1,17 @@
+/**
+ * Read configuration properties from config.properties file. <br />
+ * 
+ * @author TACKSU
+ * @since 180226
+ */
+class ProperyManager extends AbstractManager{
+    static filePath = "./config.properties"
+
+    constructor(context){
+    }
+
+    init(){
+        
+    }
+}
+module.exports = PropertyManager;
