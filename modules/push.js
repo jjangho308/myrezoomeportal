4d0c06c4d185f1e1008a1af1c930c94f4f3ea61f
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-03-02 17:57:14 +0900

    Mocha 설정 및 push test

--- modules/push.js
+++ modules/push.js
@@ -1,17 +1,73 @@
+import stompit from 'stompit'
 import AbstractManager from "./abstract";
 
 /**
  * PushManager. <br />
  * 
+ * @author 신창호.
  * @since 180228
  */
-class PushManager extends AbstractManager{
-    constructor(context){
-        super(context);
+class PushManager extends AbstractManager {
+    constructor(opt) {
+        super(opt);
     }
 
-    init(){
+    init() {}
 
+    /**
+     * Connect AMQ server. <br />
+     * 
+     * @since 180302
+     * @param {object} opt 
+     */
+    connect(opt, cb) {
+
+        this.servers = opt.servers;
+        this.targets = opt.targets;
+
+        var connections = new stompit.ConnectFailover(this.servers, {
+            maxReconnects: 1
+        });
+
+        connections.on('connecting', function (connector) {
+            var address = connector.serverProperties.remoteAddress.transportPath;
+            console.log('Connecting to ' + address);
+        })
+
+        connections.on('error', function (error) {
+            var connectArgs = error.connectArgs;
+            var address = connectArgs.host + ':' + connectArgs.port;
+
+            console.log('Connection error to ' + address + ': ' + error.message);
+        });
+
+        this.channelFactory = new stompit.ChannelFactory(connections);
+        this.channelFactory.channel(function (err, channel) {
+            if (err) {
+                console.log('channel factory error: ' + error.message);
+                return;
+            }
+
+            this.channel = channel;
+        }.bind(this))
+        cb ? cb() : null;
+    }
+
+    sendMessage(msg, cb) {
+        for(var header in this.targets){
+            this.channel.send(header, msg, function (err) {
+                if (err) {
+                    console.log('send error: ' + err.message);
+                    return;
+                }
+                console.log('sent message');
+                cb(err);
+            });
+        }
+    }
+
+    disconnect() {
+        this.channel.close();
     }
 }
 
---
+++ test/push.test.js
@@ -0,0 +1,39 @@
+import PushManager from '../modules/push'
+
+describe('Push suit', function(){
+    var push = new PushManager();
+    
+    before('Push initialize', function(){
+        push.connect({
+            targets : [{
+                'destination': '/queue/SCH',
+                'content-type': 'text/plain'
+            }],
+            servers : [
+                {
+                    host: 'b-cb8c6e8c-f893-4464-aa69-b3501991ef60-1.mq.ap-southeast-2.amazonaws.com',
+                    port: 61614,
+                    ssl: true,
+                    connectHeaders: {
+                        host: '/',
+                        login: 'rezoome',
+                        passcode: 'sgen2018!!!!'
+                    }
+                }
+            ]
+        });
+    })
+
+    it('Send message', done => {
+        this.timeout(10000);
+        push.sendMessage('Hello, world!', err =>{
+            if(!err)
+                done();
+        })
+    });
+
+    after('Diconnect AMQ', done =>{
+        push.disconnect();
+        done();
+    })
+})
