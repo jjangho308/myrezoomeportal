96e663b7951e812e4d99de84d9a89f029bb3ca10
LEEKWANGWOOK <lkwook2@gmail.com>
2018-03-06 13:53:29 +0900

    cassandra commit

--- test/db.test.js
+++ test/db.test.js
@@ -1,16 +1,61 @@
-import DatabaseManager from '../modules/db';
+//import DatabaseManager from '../modules/db';
+import AnchorDAO from '../models/anchor/anchor_dao';
+import crypto from 'crypto';
 
-describe.skip('Cassandra test suit', () => {
-    var db = null;
+describe('Cassandra test suit', () => {
+    var csdr = null;
     before('DB module initialize', () => {
+        /*
         db = new DatabaseManager();
         db.connectCsdr({
             contactPoints: ['127.0.0.1'],
             keyspace: 'rzm_anchor'
         })
+        */
+
+       csdr = new AnchorDAO('127.0.0.1','rzm_anchor');
     })
 
-    it('Put & Get transaction', done => {
+    it('Put transaction', done => {
+
+        var current_date = (new Date()).valueOf().toString();
+        var random = Math.random().toString();
+        var testTxId = crypto.createHash('sha256').update(current_date + random).digest('hex');
+
+        console.log("put data : %s, %s, %s, %s, %s, %s","LKW", testTxId, "0", "2", "OPIC", "OPIC")
+        csdr.putanchordata("LKW", testTxId, "0", "2", "OPIC", "OPIC", done2 =>{
+            console.log("====================put test====================");
+            //console.log(done2);
+            
+            if(done2!=null) {
+                console.log("insert success");
+            }
+
+            console.log("================================================");
+            
+            if(done2!=null) {
+                done();
+            }
+
+        });
+
+        /*
+        csdr.putanchordata("LKW","1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b","0","1","OPIC", done2 => {
+            console.log(done2);
+        });
+        */
+
+    })
+
+    it('GetbyTxId transaction', done => {
+        csdr.getbyTxId("421fc7b7c9fcd4e4436228af0652732d7145edf4", done2 => {
+            console.log("====================get TxId test====================");
+            console.log(done2);
+            console.log("=====================================================");
+            if(done2!=null) {
+                done();
+            }
+        });
 
     })
 
