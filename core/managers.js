76e47c41839468fe9556e46a9999a8013d8b8b67
jjangho308 <jjangho308@gmail.com>
2018-03-07 20:08:47 +0900

    commit

--- core/managers.js
+++ core/managers.js
@@ -25,7 +25,6 @@ export default (function(){
 
         push : function(){
             return pushInstance = pushInstance ? pushInstance : new PushManager();
-<<<<<<< HEAD
         },
 
         network : function(){
@@ -40,12 +39,7 @@ export default (function(){
             return cryptoInstance = cryptoInstance ? cryptoInstance : new CryptoManager();
         },
 
-        db : function(){
-=======
-        }, 
-
         database : function() {
->>>>>>> cd03ebef24c7a0284a0f59df3e6edae8e973db0d
             return dbInstance = dbInstance ? dbInstance : new DatabaseManager();
         }
     }
