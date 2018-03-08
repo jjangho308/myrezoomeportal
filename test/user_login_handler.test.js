b31a9b898b46e22abc1430573d522b6cfe43b073
pkingkong <im.pkingkong@gmail.com>
2018-03-08 17:32:58 +0900

    modify user login process test suit

--- test/user_login_handler.test.js
+++ test/user_login_handler.test.js
@@ -1,6 +1,6 @@
 import managers from '../core/managers';
 
-describe('user login process Test Suit', function () {
+describe('User Login Test Suit', function () {
 
     before('init', () => {
 
@@ -13,7 +13,6 @@ describe('user login process Test Suit', function () {
 
         // 1. 사용자 정보를 DB에서 조회
         managers.db().getUserInfo(clientReq.userid, function (res) {
-
             // 사용자 유저 존재
             if (!!res) {
                 // 비밀번호 확인
@@ -29,18 +28,20 @@ describe('user login process Test Suit', function () {
                     response.code = 200;
                     response.result = "login success";
                     // res.send(response);
-
                     console.log(response);
                     done();
                 } else {
                     // 비밀번호 실패
                     // res.send("login fail::mismatch password");
+                    console.log("login fail::mismatch password");
+                    done();
                 }
             } else {
                 // 사용자 유저 없음
                 // res.send("login fail::id not exist");
-            }
-            //}).timeout(10000);
+                console.log("login fail::id not exist");
+                done();
+            }            
         });
     });
 
