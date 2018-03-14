2d3713fcb99c0d73fd4f47d507b1fb2a72f988aa
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-03-14 15:47:40 +0900

    dist file delete

--- src/test/chai.test.js
+++ src/test/chai.test.js
@@ -7,24 +7,41 @@ import Managers from '../core/managers';
 import Initializer from '../core/initializer';
 import Util from '../util/util';
 
-describe('Chai HTTP test suite', () => {
-    before('Chai init', () => {
+describe('Portal <-> Agent Search Record interpolation test suite.', () => {
+
+    var tokenInstance = null;
+    var msgId = null;
+
+    before('Portal initailize', () => {
         chai.use(chaihttp);
+
         Initializer();
     })
 
-    it.skip('Chai GET Request test', done => {
-        chai.request(app).get('/client').end((err, res) => {
-            done();
-        });
-    })
 
-    it('Agent Request Search record', done => {
+    it('Client Request Search record', done => {
         chai.request(app)
             .post('/client')
             .set('Content-Type', 'application/json')
+            .set('Authorization', tokenInstance)
             .send({
                 cmd: 'Search',
+                args: {
+                    pkey: Util.uuid()
+                }
+            })
+            .end((err, res) => {
+                done();
+            });;
+    })
+
+    it('Agent Search result request', done => {
+        chai.request(app)
+            .post('/agent')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', tokenInstance)
+            .send({
+                cmd: 'SearchResult',
                 args: {
                     pkey: Util.uuid()
                 }
