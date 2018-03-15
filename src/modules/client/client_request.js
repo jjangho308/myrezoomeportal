fa685382443d531905f306ca235d2b440a91d016
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-03-15 18:49:38 +0900

    agent fix

--- src/modules/client/client_request.js
+++ src/modules/client/client_request.js
@@ -76,9 +76,10 @@ class ClientRequestManager extends AbstractManager {
      * @param {*} response 
      */
     response(msgId, response, cb) {
-        console.log('Stored Request' + JSON.stringify(this.requestMap));
+        console.log('Stored Request :');
+        console.log('Stored Request :' + JSON.stringify(this.requestMap));
         var entity = this.requestMap.get(msgId);
-        console.log('Stored Request' + JSON.stringify(entity));
+        console.log('Stored Request :' + JSON.stringify(entity));
         if(!!entity){
             this.handlerMap.get(entity.constructor).response(entity, response);
         }
