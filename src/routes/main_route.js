2455127e53a09ec880f0fc2134175b707e0e52ae
이광욱 <lkwook2@gmail.com>
2018-03-27 11:40:49 +0900

    main ctrl add

--- src/routes/main_route.js
+++ src/routes/main_route.js
@@ -1,9 +1,13 @@
+import express from 'express';
+import ctrl from './main_ctrl';
 /**
  * Router for './main'. <br />
  * 
  * @since 180326
  * @author TACKSU
  */
-export default (req, res, next) => {
+var router = express.Router();
 
-}
+router.get('/', ctrl.get);
+
+export default router;
