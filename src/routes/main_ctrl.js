452b19bc88e43115d8cee2c4361d5611d355cb1d
이광욱 <lkwook2@gmail.com>
2018-03-28 18:43:41 +0900

    main page modify

--- src/routes/main_ctrl.js
+++ src/routes/main_ctrl.js
@@ -8,7 +8,7 @@ export default {
         var result = [
             {
                 date: '2018-03-03 ~ 2018-04-04',
-                orglogo: '',
+                orglogo: 'https://s3.ap-northeast-2.amazonaws.com/rezoome/org_logo/t_inha05_400x400.jpg',
                 blc_map: true,
                 org_name: '인하대학교',
                 org_major: '학사, 공과대학 컴퓨터공학 전공',
@@ -16,7 +16,7 @@ export default {
             },
             {
                 date: '2018-03-03 ~ 2018-04-04',
-                orglogo: '',
+                orglogo: 'https://s3.ap-northeast-2.amazonaws.com/rezoome/org_logo/img01_mark_ab0102.gif',
                 blc_map: true,
                 org_name: '서울대학교 대학원',
                 org_major: '언론정보대학원 인터랙션디자인 전공',
