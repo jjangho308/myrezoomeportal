fa64f780c0d1caa116d5f2ec1e159fb25a16c1d3
이광욱 <lkwook2@gmail.com>
2018-04-12 11:39:15 +0900

    intro page update

---
+++ public/js/intro.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#intro_login').click(function(){
+        $('#login_div').css('visibility','visible');
+    });
+
+    $('#bt_login').click(function(){
+        //$('#login_div').css('visibility','visible');
+        //email
+        var user_email = $('#input_id').val(); 
+        
+        var user_password = $('#input_pw').val();
+
+        var param = {
+            email: user_email,
+            password: user_password
+        }
+
+        $.post("/signin", param ,function (result) {
+            console.log(result);
+        });
+        
+    });
+})
