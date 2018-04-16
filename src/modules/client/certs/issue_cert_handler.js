70fa3f7255c1e390fcc043a317de08931bafa96c
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-04-16 14:53:20 +0900

    Certificate ID-> UUID

--- src/modules/client/certs/issue_cert_handler.js
+++ src/modules/client/certs/issue_cert_handler.js
@@ -36,6 +36,7 @@ class IssueCertificatHandler extends AbstractClientRequestHandler {
      */
     request(request, cb) {
         var certModel = new CertModel({
+            certId : Util.uuid(),
             uId: request.uId,
             blcMapId: request.cert.txid
         });
