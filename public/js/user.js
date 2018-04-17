5c2aa13db54463a444b4f28e9bd342ae8857bc7b
pkingkong <im.pkingkong@gmail.com>
2018-04-17 17:37:44 +0900

    remove unused file

--- public/js/user.js
+++ public/js/user.js
@@ -1,24 +1,22 @@
-$(document).ready(function(){
-    
-    $('#signin').click(function(){
+$(document).ready(function () {
+    $('#signin').click(function () {
         window.location.href = "/signin";
     });
 
-    $("#signup").click(function(){
+    $("#signup").click(function () {
         window.location.href = "/signup";
     });
 
-    $('#btn_signin').click(function(){
-
-        if($('#signin_id').val() == '') {
+    $('#btn_signin').click(function () {
+        if ($('#signin_id').val() == '') {
             alert("id empty");
             return;
-        } else if($('#signin_pw').val() == '') {
+        } else if ($('#signin_pw').val() == '') {
             alert("pw empty");
             return;
         }
 
-        var user_email = $('#signin_id').val();        
+        var user_email = $('#signin_id').val();
         var user_password = SHA256($('#signin_pw').val());
 
         var param = {
@@ -26,35 +24,32 @@ $(document).ready(function(){
             password: user_password
         }
 
-        $.post("/signin", param ,function (result) {
+        $.post("/signin", param, function (result) {
             console.log(result);
             window.location.href = "main";
         });
-        
     });
 
-
-    $('#btn_signup').click(function(){
-
-        if($('#signup_id').val() == '') {
+    $('#btn_signup').click(function () {
+        if ($('#signup_id').val() == '') {
             alert("id empty");
             return;
-        } else if($('#signup_pw').val() == '') {
+        } else if ($('#signup_pw').val() == '') {
             alert("pw empty");
             return;
         }
 
-        var user_email = $('#signup_id').val();        
+        var user_email = $('#signup_id').val();
         var user_password = SHA256($('#signup_pw').val());
         var user_password_confirm = SHA256($('#signup_pw_confirm').val());
-        var familyname = '홍';//$('#signup_familyname').val();
-        var firstname = '길동';//$('#signup_firstname').val();
-        var gender = 'M';//$('#signup_gender').val();
-        var birth = '20001234';//$('#signup_birth').val();
-        var phone = '01055557777';//$('#signup_phone').val();
-        var carrier_name = "SKT";        
+        var familyname = $('#signup_familyname').val();
+        var firstname = $('#signup_firstname').val();
+        var gender = $('#signup_gender').val();
+        var birth = $('#signup_birth').val();
+        var phone = $('#signup_phone').val();
+        var carrier_name = "SKT";
 
-        if(user_password != user_password_confirm) {
+        if (user_password != user_password_confirm) {
             alert("비밀번호가 다르다");
             return;
         }
@@ -64,7 +59,7 @@ $(document).ready(function(){
             pw: user_password,
             familyNameKO: familyname,
             firstNameKO: firstname,
-            fullNameKO: familyname+firstname,
+            fullNameKO: familyname + firstname,
             birth: birth,
             phone: phone,
             gender: gender,
@@ -75,6 +70,5 @@ $(document).ready(function(){
             console.log(result);
             window.location.href = "signin";
         });
-        
     });
 });
