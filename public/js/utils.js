659b03a44bb203d337120c5e059f10cc13b84680
이광욱 <lkwook2@gmail.com>
2018-04-24 16:03:27 +0900

    resume page modify

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,11 +1,39 @@
 var socket;
 var client_token;
 
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
+}
+
+
+function getTimeStamp() {
+    var d = new Date();
+
+    var s =
+        leadingZeros(d.getFullYear(), 4) + '-' +
+        leadingZeros(d.getMonth() + 1, 2) + '-' +
+        leadingZeros(d.getDate(), 2) + ' ' +
+
+        leadingZeros(d.getHours(), 2) + ':' +
+        leadingZeros(d.getMinutes(), 2) + ':' +
+        leadingZeros(d.getSeconds(), 2);
+
+    return s;
+}
+
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -19,7 +47,7 @@ function getCookie(cname) {
 
 function setData(record) {
     // dcript data 
-    
+
     sessionStorage.setItem(record.txid, JSON.stringify(record));
 
     addTxidList(record.txid);
@@ -31,10 +59,10 @@ function getData(record_txid) {
 
 function addTxidList(txid) {
     var txidlist = getTxidList();
-    
+
     //중복제거 로직 
-    for(var i in txidlist) {
-        if(txidlist[i]==txid) {
+    for (var i in txidlist) {
+        if (txidlist[i] == txid) {
             return;
         }
     }
@@ -48,32 +76,32 @@ function setTxidList(txidarray) {
 }
 
 function getTxidList() {
-    var storagedata =  sessionStorage.getItem(client_token);
+    var storagedata = sessionStorage.getItem(client_token);
     var resultarray = storagedata.split(",");
     return resultarray;
 }
 
-function SHA256(s){      
-    var chrsz   = 8;
+function SHA256(s) {
+    var chrsz = 8;
     var hexcase = 0;
-  
-    function safe_add (x, y) {
+
+    function safe_add(x, y) {
         var lsw = (x & 0xFFFF) + (y & 0xFFFF);
         var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
         return (msw << 16) | (lsw & 0xFFFF);
     }
-  
-    function S (X, n) { return ( X >>> n ) | (X << (32 - n)); }
-    function R (X, n) { return ( X >>> n ); }
+
+    function S(X, n) { return (X >>> n) | (X << (32 - n)); }
+    function R(X, n) { return (X >>> n); }
     function Ch(x, y, z) { return ((x & y) ^ ((~x) & z)); }
     function Maj(x, y, z) { return ((x & y) ^ (x & z) ^ (y & z)); }
     function Sigma0256(x) { return (S(x, 2) ^ S(x, 13) ^ S(x, 22)); }
     function Sigma1256(x) { return (S(x, 6) ^ S(x, 11) ^ S(x, 25)); }
     function Gamma0256(x) { return (S(x, 7) ^ S(x, 18) ^ R(x, 3)); }
     function Gamma1256(x) { return (S(x, 17) ^ S(x, 19) ^ R(x, 10)); }
-  
-    function core_sha256 (m, l) {
-         
+
+    function core_sha256(m, l) {
+
         var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1,
             0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
             0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786,
@@ -91,11 +119,11 @@ function SHA256(s){
         var W = new Array(64);
         var a, b, c, d, e, f, g, h, i, j;
         var T1, T2;
-  
+
         m[l >> 5] |= 0x80 << (24 - l % 32);
         m[((l + 64 >> 9) << 4) + 15] = l;
-  
-        for ( var i = 0; i<m.length; i+=16 ) {
+
+        for (var i = 0; i < m.length; i += 16) {
             a = HASH[0];
             b = HASH[1];
             c = HASH[2];
@@ -104,14 +132,14 @@ function SHA256(s){
             f = HASH[5];
             g = HASH[6];
             h = HASH[7];
-  
-            for ( var j = 0; j<64; j++) {
+
+            for (var j = 0; j < 64; j++) {
                 if (j < 16) W[j] = m[j + i];
                 else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
-  
+
                 T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
                 T2 = safe_add(Sigma0256(a), Maj(a, b, c));
-  
+
                 h = g;
                 g = f;
                 f = e;
@@ -121,7 +149,7 @@ function SHA256(s){
                 b = a;
                 a = safe_add(T1, T2);
             }
-  
+
             HASH[0] = safe_add(a, HASH[0]);
             HASH[1] = safe_add(b, HASH[1]);
             HASH[2] = safe_add(c, HASH[2]);
@@ -133,28 +161,28 @@ function SHA256(s){
         }
         return HASH;
     }
-  
-    function str2binb (str) {
+
+    function str2binb(str) {
         var bin = Array();
         var mask = (1 << chrsz) - 1;
-        for(var i = 0; i < str.length * chrsz; i += chrsz) {
-            bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i%32);
+        for (var i = 0; i < str.length * chrsz; i += chrsz) {
+            bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32);
         }
         return bin;
     }
-  
+
     function Utf8Encode(string) {
-        string = string.replace(/\r\n/g,"\n");
+        string = string.replace(/\r\n/g, "\n");
         var utftext = "";
-  
+
         for (var n = 0; n < string.length; n++) {
-  
+
             var c = string.charCodeAt(n);
-  
+
             if (c < 128) {
                 utftext += String.fromCharCode(c);
             }
-            else if((c > 127) && (c < 2048)) {
+            else if ((c > 127) && (c < 2048)) {
                 utftext += String.fromCharCode((c >> 6) | 192);
                 utftext += String.fromCharCode((c & 63) | 128);
             }
@@ -163,22 +191,22 @@ function SHA256(s){
                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
                 utftext += String.fromCharCode((c & 63) | 128);
             }
-  
+
         }
-  
+
         return utftext;
     }
-  
-    function binb2hex (binarray) {
+
+    function binb2hex(binarray) {
         var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
         var str = "";
-        for(var i = 0; i < binarray.length * 4; i++) {
-            str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
-            hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
+        for (var i = 0; i < binarray.length * 4; i++) {
+            str += hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) +
+                hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF);
         }
         return str;
     }
-  
+
     s = Utf8Encode(s);
-    return binb2hex(core_sha256(str2binb(s), s.length * chrsz));  
+    return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
 }
