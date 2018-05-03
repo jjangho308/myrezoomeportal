9c2ff7ed57fd661f3a7627579181170dc716c6ce
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-05-03 19:10:23 +0900

    Signin router testcode fix

--- src/test/signin.test.js
+++ src/test/signin.test.js
@@ -17,9 +17,10 @@ describe('Sign In test suite', () => {
         chai.request(app)
             .post('/signin')
             .set('Content-Type', 'application/json')
+            .set('X-Requested-With', 'XMLHttpRequest')
             .send({
-                email: 'exle_@naver.com',
-                password: '1234'
+                email: 'test@naver.com',
+                pw: 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'
             })
             .end((err, res) => {
                 done();
