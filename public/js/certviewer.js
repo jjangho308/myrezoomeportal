33b40e4f7666b0e88a9ad29e854361c20f5983c9
이광욱 <lkwook2@gmail.com>
2018-05-08 14:01:52 +0900

    certview modify

--- public/js/certviewer.js
+++ public/js/certviewer.js
@@ -210,11 +210,10 @@ var certformatter= {
         // opic
         var htmldiv = '<div>';
             htmldiv = htmldiv + "<p>이름 : " +record_data.name + '</p>';
+            htmldiv = htmldiv + "<p>testid : " +record_data.testid + '</p>';
+            htmldiv = htmldiv + "<p>고유번호 : " +record_data.phone + '</p>';
+            htmldiv = htmldiv + "<p>시험일 : " +record_data.date + '</p>';
             htmldiv = htmldiv + "<p>Grade : " +record_data.grade + '</p>';
-            htmldiv = htmldiv + "<p>총점 : " +record_data.point0 + '</p>';
-            htmldiv = htmldiv + "<p>과목1 : " +record_data.point1 + '</p>';
-            htmldiv = htmldiv + "<p>과목2 : " +record_data.point2 + '</p>';
-            htmldiv = htmldiv + "<p>과목3 : " +record_data.point3 + '</p>';
             
         htmldiv = htmldiv + '</div>';
         $('#cert-body-div').append(htmldiv);
