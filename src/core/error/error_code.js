9420bbaa9f795bb6b4bad6b333f7be7bed379bd3
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-05-08 17:24:54 +0900

    Error code container created.

---
+++ src/core/error/error_code.js
@@ -0,0 +1,15 @@
+/**
+ * Error code container. <br />
+ * 
+ * 
+ * @since 180508
+ * @author TACKSU
+ */
+export default {
+
+    // TODO Add error code status.
+
+    AUTHENTICATION_ERROR : 100,
+    AUTH_NO_EMAIL : 101,
+    AUTH_PASSWORD_INCORRECT : 102
+}
