7ea6a93c66ec6d7ae83acf73074c80315de53a37
이광욱 <lkwook2@gmail.com>
2018-05-16 15:05:44 +0900

    verify js modify

--- public/js/verifypage.js
+++ public/js/verifypage.js
@@ -1,3 +1,7 @@
 function test1() {
-    
+    var decrypted = CryptoJS.AES.decrypt(omsg.records[i].data, CryptoJS.enc.Base64.parse(
+        decryptedKey), {
+        iv: CryptoJS.enc.Base64.parse(recv_iv)
+        });
+        console.log(decrypted.toString(CryptoJS.enc.Utf8));
 }
