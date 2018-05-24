b1895e038847326c5336cd60cee1a894532b4b54
pkingkong <im.pkingkong@gmail.com>
2018-05-24 14:44:24 +0900

    modify

--- public/js/share-cert.js
+++ public/js/share-cert.js
@@ -16,49 +16,56 @@ $(document).ready(function(){
 });
 
 function verify(passcode) {
-	var passcodehash = hexToBase64(SHA256(passcode));
 
-	try {
-		if(certdata.encrypted) {
-			var encodedIv = certdata.iv;
-			var encryptedData = certdata.data;
-
-			var decrypted = CryptoJS.AES.decrypt(encryptedData, CryptoJS.enc.Hex.parse(
-				passcodehash), {
-				iv: CryptoJS.enc.Base64.parse(encodedIv)
-			});
-			console.log(decrypted.toString(CryptoJS.enc.Utf8));
-
-			//process verify
-			var json_decrypted = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));        
-        	certformatter[json_decrypted.subid](json_decrypted.data);
-			
-
-			$(".main-container").css("display", "none");
-    		$(".loading-container").css("display", "block");
-
-		    var current_active = 0;
-		    
-		    $(`#circle-${current_active}`).css("background-color","#4a90e2");
-		    
-		    setInterval(function(){
-		        $(`#circle-${current_active}`).css("background-color","#dadada");
-		        current_active += 1;
-		        
-		        if(current_active > 5){
-					current_active = 0;
-					$("#cert-verify").css("display", "none");
-					$("#cert-viewer").css("display", "block");
-		        }
-		        $(`#circle-${current_active}`).css("background-color","#4a90e2");
-		                           
-		                          
-		     }, 1000);
-		}
-	}
-	catch(exception) {
+	if(hexToBase64(SHA256(passcode)) != certdata.passcode) {
 		$("input").css("border", "solid 1px #f59188");
 		$(".error-message").css("display", "block");
+	} else {
+
+		var passcodehash = SHA256(passcode);
+
+		try {
+			if(certdata.encrypted) {
+				var encodedIv = certdata.iv;
+				var encryptedData = certdata.data;
+
+				var decrypted = CryptoJS.AES.decrypt(encryptedData, CryptoJS.enc.Hex.parse(
+					passcodehash), {
+					iv: CryptoJS.enc.Base64.parse(encodedIv)
+				});
+				console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+				//process verify
+				var json_decrypted = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));        
+				certformatter[json_decrypted.subid](json_decrypted.data);
+				
+
+				$(".main-container").css("display", "none");
+				$(".loading-container").css("display", "block");
+
+				var current_active = 0;
+				
+				$(`#circle-${current_active}`).css("background-color","#4a90e2");
+				
+				setInterval(function(){
+					$(`#circle-${current_active}`).css("background-color","#dadada");
+					current_active += 1;
+					
+					if(current_active > 5){
+						current_active = 0;
+						$("#cert-verify").css("display", "none");
+						$("#cert-viewer").css("display", "block");
+					}
+					$(`#circle-${current_active}`).css("background-color","#4a90e2");									
+									
+				}, 1000);
+			}
+		}
+		catch(exception) {
+			$("input").css("border", "solid 1px #f59188");
+			$(".error-message").css("display", "block");
+		}
+
 	}
 }
 
