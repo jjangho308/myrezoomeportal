d32c36e93bbc0f3131c476ca39fbbf4fbd729bd0
이광욱 <lkwook2@gmail.com>
2018-06-06 14:28:13 +0900

    mainpage modify

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -757,6 +757,46 @@ function clientsocket_listener() {
     });
 }
 
+function firstLogin() {
+    $(".section-1 button").click(function(){
+    	$('.section-1').css("display", "none");
+    	$('.section-3').css("display", "block");
+
+
+	    setTimeout(function() {
+	         $('.ko-progress-circle').attr('data-progress', 20);
+	         $('.percentage span').text("20%");
+	    }, 100);
+	    setTimeout(function() {
+	         $('.ko-progress-circle').attr('data-progress', 50);
+	         $('.percentage span').text("50%");
+	    }, 1000);
+	    setTimeout(function() {
+	         $('.ko-progress-circle').attr('data-progress', 100);
+	         $('.percentage span').text("100%");
+
+		     setTimeout(function() {
+		     	$('.percentage span').css("display", "none");
+		     	$('.section-3 button').prop("disabled", false);
+		     	
+		     	$('.percentage img').css("display", "block");
+
+		    }, 1000);
+           
+            
+
+	    }, 2000);
+    });
+    
+    $(".section-3 button").click(function(){
+    	$('#initial-dialog .close-modal').click();
+    });
+
+    $('#initial-dialog').modal('show');
+
+    
+}
+
 function setSocket(mId) {
     socket.close();
     socket = io();
