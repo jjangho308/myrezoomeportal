7003da9f735be7ffc9b00df335da57a724f7e591
이광욱 <lkwook2@gmail.com>
2018-06-12 11:05:37 +0900

    sign out add

---
+++ src/routes/signout_route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import ctrl from './signout_ctrl';
+
+/**
+ * Router for /signin URI. <br />
+ * 
+ * @since 180322
+ * @author TACKSU
+ */
+var router = express.Router();
+
+router.get('/', ctrl.get);
+//router.post('/', ctrl.post);
+// router.use('/', (err, req, res, next)=>{
+//     console.log(err.toString());
+// })
+
+export default router;
