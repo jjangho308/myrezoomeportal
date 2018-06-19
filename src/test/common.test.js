0a13f0caeb9e61f3bee1e94de36b41dcec8b94b8
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-06-19 20:40:07 +0900

    Http client request test code complete

--- src/test/common.test.js
+++ src/test/common.test.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var managers = require('../core/managers');
 var jsonminify = require('jsonminify');
 
+var httpclient = require('http');
+
 /**
  * Instance test suit. <br />
  * 
@@ -13,7 +15,54 @@ describe('Instant test suit', () => {
         process.env.NODE_ENV = 'development';
     });
 
-    it('Error catch', () => {
+    it('Httpclient request', done => {
+        // httpclient.get('http://devadminexternalelb-2109283886.ap-northeast-2.elb.amazonaws.com:28080/monitor/getTransactionByTXId?tx_id="58af4a7836336521014d45ad0d6046303d6c730e9e16d1939f0c99e17ae234bb"', (response) => {
+        //     var chunk = ''
+        //     response.on('data', data => {
+        //         chunk += data;
+        //     });
+
+        //     response.on('end', (end) => {
+        //         console.log(chunk);
+        //     });
+        // }).on("error", (err) => {
+        //     console.log("Error: " + err.message);
+        // });
+
+        var req = httpclient.request({
+            hostname: 'devadminexternalelb-2109283886.ap-northeast-2.elb.amazonaws.com',
+            port : 28080,
+            method: 'GET',
+            path: '/monitor/getTransactionByTXId?tx_id="58af4a7836336521014d45ad0d6046303d6c730e9e16d1939f0c99e17ae234bb',
+            headers: {
+                'handsome': 'kyc'
+            }
+        }, (response) => {
+            var data = '';
+            response.on('data', chunk => {
+                data += Buffer.from(chunk).toString();
+                console.log(chunk);
+            });
+
+            response.on('end', end => {
+                console.log(data);
+            });
+        });
+        req.end();
+
+        // req.on('connect', (res, socket, head) => {
+        //     console.log('connected');
+        //     socket.write('/GET /monitor/getTransactionByTXId?tx_id="58af4a7836336521014d45ad0d6046303d6c730e9e16d1939f0c99e17ae234bb\r\n"' +
+        //         'Host : 2.elb.amazonaws.com:28080\r\n' +
+        //         '\r\n\r\n');
+        //     socket.on('data', (chunk) => {
+        //         console.log(chunk);
+        //         done();
+        //     })
+        // });
+    })
+
+    it.skip('Error catch', () => {
         try {
             throw new Error("test error");
         } catch (err) {
