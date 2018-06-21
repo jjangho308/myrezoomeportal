912fff25bbe3564ac2641dbf2f7bce714f513840
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-06-21 13:43:20 +0900

    local, development, stage, production 4 environment로 분리

--- src/core/environment.js
+++ src/core/environment.js
@@ -2,11 +2,18 @@
  * Environment checker. <br />
  */
 module.exports = {
+    local: (() => {
+        return process.env.NODE_ENV === 'local';
+    }),
 
     developement: (() => {
         return process.env.NODE_ENV === 'development';
     })(),
 
+    stage: (() => {
+        return process.env.NODE_ENV === 'stage';
+    }),
+    
     prouction: (() => {
         return process.env.NODE_ENV === 'production';
     })(),
