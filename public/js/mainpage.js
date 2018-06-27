faf6d41d0794b6362d11285f454b97358fd7c751
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-06-27 11:08:40 +0900

    onload => ready convert

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -3,12 +3,47 @@ require socket.is
 <script src="/socket.io/socket.io.js"></script>
 */
 
+/**
+ * Ready initializer. <br />
+ */
 $(document).ready(function () {
+
+    var client_token = window.client_token = getCookie("JWT");
+    var client_authorization = window.client_authorization = 'Bearer ' + client_token;
+
+    socket = io();
+    //request to agent for get user info
+    var pagetxidlist = getTxidList();
+
+    if (pagetxidlist.length > 1) {
+        //sessing storage have user info (txid list)
+        var oridata = [];
+
+        for (var i = 0; i < pagetxidlist.length; i++) {
+            try {
+                var objuserdata = getData(pagetxidlist[i]);
+                oridata.push(objuserdata);
+            } catch (exception) {
+                console.error(exception);
+                continue;
+            }
+        }
+        $('.spec-body-default').show();
+        refreshview(oridata);
+        $('#initial-dialog .close-modal').click();
+    } else {
+        //session storage dont have user info(txid list)
+        genRsaKey(function (err, keypair) {
+            if (!!err) {
+                console.error(JSON.stringify(err));
+            } else if (!!keypair) {
+                request_agent();
+            }
+        });
+    }
+
     $(".study-period").datepicker();
     $(".study-period").datepicker("option", "dateFormat", "yy-mm-dd");
-
-    client_token = getCookie("JWT");
-    client_authorization = 'Bearer ' + client_token;
 
     $('.spec-body-loading').show();
     $('.spec-body-default').hide();
@@ -108,9 +143,6 @@ $(document).ready(function () {
         }
         $("select").selectize();
     });
-
-
-
 
     $('#education-add-dialog .confirm-btn').click(function () {
 
@@ -280,6 +312,7 @@ $(document).ready(function () {
     });
 
     $('#language-add-dialog .confirm-btn').click(function () {
+        debugger;
         var issuer = $("#language-issuer").val();
         var lang = $("#langadd_lang").val();
         var name = $("#language-name").val();
@@ -756,46 +789,8 @@ $(document).ready(function () {
         }
     }, true);
 
-});
-
-
-
-window.onload = function () {
-    socket = io();
-    //request to agent for get user info
-    var pagetxidlist = getTxidList();
-
-    if (pagetxidlist.length > 1) {
-        //sessing storage have user info (txid list)
-        var oridata = [];
-
-        for (var i = 0; i < pagetxidlist.length; i++) {
-            try {
-                var objuserdata = getData(pagetxidlist[i]);
-                oridata.push(objuserdata);
-
-            } catch (exception) {
-                console.error(exception);
-                continue;
-            }
-
-        }
-        $('.spec-body-default').show();
-        refreshview(oridata);
-        $('#initial-dialog .close-modal').click();
-    } else {
-        //session storage dont have user info(txid list)
-        genRsaKey(function (err, keypair) {
-            if (!!err) {
-                console.error(JSON.stringify(err));
-            } else if (!!keypair) {
-                request_agent();
-            }
-        });
-    }
-
     getPrivateRecords();
-};
+});
 
 function change_default_cert(subid) {
     $(".change_cert").remove();
@@ -1135,7 +1130,6 @@ function firstLogin() {
         $('.section-1').css("display", "none");
         $('.section-3').css("display", "block");
 
-
         setTimeout(function () {
             $('.ko-progress-circle').attr('data-progress', 20);
             $('.percentage span').text("20%");
@@ -1155,8 +1149,6 @@ function firstLogin() {
                 $('.percentage img').css("display", "block");
 
             }, 1000);
-
-
 
         }, 2000);
     });
