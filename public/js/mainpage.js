d24d2f23b018886df151253de54318eccbb51feb
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-06-27 15:26:08 +0900

    Loading start 위치 변경

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
 
     socket = io();
 
-    startLoading();
     //request to agent for get user info
     var pagetxidlist = getTxidList();
 
@@ -33,6 +32,7 @@ $(document).ready(function () {
         refreshview(oridata);
         $('#initial-dialog .close-modal').click();
     } else {
+        startLoading();
         //session storage dont have user info(txid list)
         genRsaKey(function (err, keypair) {
             if (!!err) {
@@ -156,7 +156,7 @@ $(document).ready(function () {
         $('#education-add-dialog .confirm-btn').click(function () {
 
             var is_error = false;
-    
+
             if ($("#school").val() == "") {
                 $("#school").addClass("error");
                 $("#school").next().css("display", "block");
@@ -165,12 +165,12 @@ $(document).ready(function () {
                 $("#school").removeClass("error");
                 $("#school").next().css("display", "none");
             }
-    
+
             $(".major").each(function () {
                 var element = $(this);
-    
+
                 var range = element.closest(".error-range");
-    
+
                 if (element.val() == "") {
                     element.addClass("error");
                     range.find(".items ").addClass("error");
@@ -183,60 +183,58 @@ $(document).ready(function () {
                     range.find(".error-message").css("display", "none");
                 }
             })
-    
+
             var period = $("#education-add-dialog .study-period");
             var range = period.closest(".error-range");
-    
+
             var date1 = new Date(period[0].value);
             var date2 = new Date(period[1].value);
-    
+
             if ((period[0].value == "") || (period[1].value == "")) {
                 period.addClass("error");
                 range.find("button").addClass("error");
                 range.find(".items").addClass("error");
                 range.find(".error-message").css("display", "block");
                 is_error = true;
-            }else if(date1 - date2 > 0){
-                is_error= true;
+            } else if (date1 - date2 > 0) {
+                is_error = true;
                 period.addClass("error");
                 range.find("button").addClass("error");
                 range.find(".items").addClass("error");
                 $("#education-add-dialog #error-range-period").text("시작일이 종료일 보다 클 수 없습니다.");
                 range.find(".error-message").css("display", "block");
-            }
-            else {
+            } else {
                 period.removeClass("error");
                 range.find("button").removeClass("error");
                 range.find(".items").removeClass("error");
-                range.find(".error-message").css("display", "none");  
+                range.find(".error-message").css("display", "none");
             }
-    
+
             var range = $("#score").closest(".error-range");
             if ($("#score").val() == "") {
                 $("#score").addClass("error");
                 range.find(".items").addClass("error");
                 range.find(".error-message").css("display", "block");
                 is_error = true;
-            }else if(!$.isNumeric($("#education-add-dialog #score").val())){
+            } else if (!$.isNumeric($("#education-add-dialog #score").val())) {
                 $("#score").addClass("error");
                 range.find(".items").addClass("error");
                 $("#education-add-dialog #score-error-message").text("학점은 숫자만 입력 가능합니다.");
                 range.find(".error-message").css("display", "block");
                 is_error = true;
-            } 
-            else {
+            } else {
                 $("#score").removeClass("error");
                 range.find(".items").removeClass("error");
                 range.find(".error-message").css("display", "none");
             }
-    
+
             var school_name = $("#school").val();
             var degree = $("#score").val();
             var total_degree = $("#total_score").val();
             var start_date = $("#edu-startdate").val();
             var end_date = $("#edu-enddate").val();
             var status = $("#study-field").val();
-    
+
             // cert format
             var param = {
                 school_name: school_name,
@@ -245,10 +243,10 @@ $(document).ready(function () {
                 enddate: end_date,
                 status: status
             }
-    
+
             // cert encryption
             var enc_record = JSON.stringify(param);
-    
+
             if (!is_error) {
                 $.ajax({
                     type: 'POST',
@@ -270,15 +268,15 @@ $(document).ready(function () {
                         $("#alarm-div span").text("정상적으로 입력 완료되었습니다.");
                         $('#alarm-div').css("display", "block");
                         $('#alarm-div').css("margin-right", "-108px");
-    
+
                         setTimeout(function () {
                             $('#alarm-div').fadeOut('slow');
                         }, 2000);
-    
+
                         //clean view
                         $('.private-spec-body').remove();
                         $('#spec_edu_detail > .spec-body-default').hide();
-    
+
                         getPrivateRecords();
                     },
                     contentType: 'application/json',
@@ -800,7 +798,7 @@ $(document).ready(function () {
         });
 
         document.getElementById("spec_edu_detail_targetdiv").addEventListener("record_updated", function (event) {
-            
+
             event.stopPropagation();
             event.preventDefault();
 
@@ -810,7 +808,7 @@ $(document).ready(function () {
         }, true);
 
         document.getElementById("spec_certification_targetdiv").addEventListener("record_updated", function (event) {
-            
+
             event.stopPropagation();
             event.preventDefault();
 
@@ -820,7 +818,7 @@ $(document).ready(function () {
         }, true);
 
         document.getElementById("spec_forign_lang_targetdiv").addEventListener("record_updated", function (event) {
-            
+
             event.stopPropagation();
             event.preventDefault();
 
