97df3901f4d1b7907f6d4f4dedc71602f1492f8f
이광욱 <lkwook2@gmail.com>
2018-06-29 10:53:59 +0900

    mainpage modify

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -792,12 +792,10 @@ $(document).ready(function () {
                 if ($(event.target.parentNode.parentNode)[0].className == "spec-body" && $(event.target.parentNode.parentNode).find("input:checkbox:not(:checked)").length == 1) {
                     var parentparentNodeChecknot = $(event.target.parentNode.parentNode).find("input:checkbox:not(:checked)");
                     parentparentNodeChecknot[0].checked = true;
-                    parentparentNodeChecknot[1].checked = true;
                     event.preventDefault();
                 } else if ($(event.target.parentNode.parentNode)[0].className == "spec-body" && $(event.target.parentNode.parentNode).find("input:checkbox:checked").length == 1) {
                     var parentparentNodeChecknot = $(event.target.parentNode.parentNode).find("input:checkbox:checked");
                     parentparentNodeChecknot[0].checked = false;
-                    parentparentNodeChecknot[1].checked = false;
                     event.preventDefault();
                 }
 
